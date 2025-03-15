c5fb7be0bf16644150bfefd3aea492ad1f005090
IldarSaygafarov2 <sayildar17@gmail.com>
2025-03-15 11:53:52 +0500

    added fixes

--- app/views.py
+++ app/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def render_home_page(request):
     return render(request, "app/index.html")
+
+
+def render_contact_page(request):
+    return render(request, "app/contact.html")
+
+
+def render_about_page(request):
+    return render(request, "app/about.html")
+
+
+def render_services_page(request):
+    return render(request, "app/services.html")
