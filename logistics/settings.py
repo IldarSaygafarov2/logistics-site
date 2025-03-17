5a764cef8ef9da53c3cfc747a5c1f08f22d5d71f
IldarSaygafarov2 <sayildar17@gmail.com>
2025-03-17 18:30:29 +0500

    added fixes

--- logistics/settings.py
+++ logistics/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = "django-insecure-8l4@47)=!#&_06vkd5ii$!bgn4xw5+hq)u!ix#*ehs2r5)6@hn
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["newavialogistic.uz", "127.0.0.1"]
+ALLOWED_HOSTS = ["newavialogistic.uz"]
 
 CSRF_TRUSTED_ORIGINS = ["https://newavialogistic.uz"]
 
@@ -113,8 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "app/static"]
+STATIC_ROOT = BASE_DIR / "app/static"
 
 
 MEDIA_URL = "media/"
