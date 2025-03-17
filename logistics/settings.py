1257aa1126e0949dda5347d256cf2a9cd5f90752
IldarSaygafarov2 <sayildar17@gmail.com>
2025-03-17 17:50:32 +0500

    added fixes

--- logistics/settings.py
+++ logistics/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_DIR = BASE_DIR / "app/static"
+STATIC_ROOT = BASE_DIR / "app/static"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
